040f6c3354bbeed1ad1af68f3a14efc87ffb1a7e
Hamer Sun <hsun3@logitech.com>
2025-10-30 16:10:14 +0800

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import App2 from './App.tsx'
+import App2 from './App2.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
